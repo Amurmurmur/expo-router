7b97313ce715b36dbaba8ce420780560f4f6ac57
Amurmurmur <amurmurmur@gmail.com>
2024-01-25 00:05:05 +0000

    chore: make it insanely minimal

--- app/(tabs)/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from 'react-native'
-
-export default function TabOneScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: 'center' }} >
-      <Text style={{ fontSize: 50, color: 'red'}}>Tab One</Text>
-    </View>
-  )
-}
--- app/(tabs)/two.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from 'react-native'
-
-export default function TabTwoScreen() {
-  return (
-    <View style={{ flex: 1 }}>
-      <Text style={{ fontSize: 20 }}>Tab Two</Text>
-    </View>
-  )
-}
